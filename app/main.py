886b6681bcbead1c7c8882be6abac169a43d0810
maksim-backend-dev <maksim.backend.dev@gmail.com>
2025-04-25 19:00:44 +0300

    solved a trouble with launching after changing the structure of the project

--- app/main.py
+++ app/main.py
@@ -11,5 +11,6 @@ models.Base.metadata.create_all(bind=engine)
 app = FastAPI()
 app.include_router(router)
 
+# This is for Docker container. Locally use "uvicorn app.main:app --reload --port 8080 --host 0.0.0.0" (root dir)
 if __name__ == "__main__":
     uvicorn.run( "main:app", host='0.0.0.0', port=8000, reload=True)
